a4ceaa8c2c8116a1f1e4e9c7536d8ca3331ab306
palmleon <leonardo.palmucci@gmail.com>
2021-11-03 14:40:13 +0100

    feat: add script for incremental Timeout Policy

--- Lab 1/dpm-simulator/incrementalTimeoutDPM.sh
+++ Lab 1/dpm-simulator/incrementalTimeoutDPM.sh
@@ -2,6 +2,11 @@
 
 psm="example/psm.txt"
 wl="../workloads/workload_1.txt"
-
+outputfile="EnergyStats.txt"
+>$outputfile
+for timeout in {0..1000}; do
+	printf "%d " $timeout >>$outputfile
+	./dpm_simulator.exe -t $timeout -psm $psm -wl $wl | grep -o " Energy w DPM.*" | egrep -o "[0-9]+\.[0-9]+" >>$outputfile
+done
 
 exit 0
