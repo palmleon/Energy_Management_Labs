9299145e86d20263655b33a19021e31a217c50d1
palmleon <leonardo.palmucci@gmail.com>
2021-11-03 15:26:23 +0100

    feat: incrementalTimeout now extracts #transitions and Time overhead for transition

--- Lab 1/dpm-simulator/incrementalTimeoutDPM.sh
+++ Lab 1/dpm-simulator/incrementalTimeoutDPM.sh
@@ -2,11 +2,19 @@
 
 psm="example/psm.txt"
 wl="../workloads/workload_1.txt"
-outputfile="EnergyStats.txt"
->$outputfile
-for timeout in {0..1000}; do
-	printf "%d " $timeout >>$outputfile
-	./dpm_simulator.exe -t $timeout -psm $psm -wl $wl | grep -o " Energy w DPM.*" | egrep -o "[0-9]+\.[0-9]+" >>$outputfile
+stats="EnergyStats.txt"
+tmpfile="tmpfile.txt"
+>$stats
+for timeout in {0..10}; do
+	./dpm_simulator.exe -t $timeout -psm $psm -wl $wl >$tmpfile
+	printf "%d " $timeout >>$stats
+	grep -o "Energy w DPM.*" <$tmpfile | egrep -o "[0-9]+\.[0-9]+" | tr -d '\n'>>$stats
+	printf " " >>$stats
+	grep "Time overhead for transition" <$tmpfile | egrep -o "[0-9]+\.[0-9]+" | tr -d '\n'>> $stats
+	printf " " >>$stats
+	fgrep "N. of transition" <$tmpfile | egrep -o "[0-9]+" >> $stats 
 done
+
+rm -f $tmpfile
 
 exit 0
